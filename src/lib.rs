dab839f876dbd6c635e5ddb1e095d87c4105d857
MARC Giovanni <gvnnmarc@gmail.com>
2024-12-20 10:55:13 +0100

    integration of redis to manage and save data from different workflows

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 wit_bindgen::generate!({ generate_all }); //  génère automatiquement des bindings Rust pour un monde WIT
-
+use wasmcloud_component::wasi::keyvalue::*;
 use wasmcloud_component::{http::{self, ErrorCode, IncomingBody, OutgoingBody, Request, Response, Server}, info};
 use serde::{Deserialize, Serialize};
-use std::sync::{Arc, Mutex};
 use uuid::Uuid;
 use std::io::Read;
-use lazy_static::lazy_static;
 
 use example::{add::adder::add, print::printer::print as printer, wait::waiter::wait};
 
@@ -21,25 +19,39 @@ struct Workflow {
     task: Vec<Task>,
 }
 
-#[derive(Serialize, Clone)]
+#[derive(Debug, Serialize, Clone, serde::Deserialize)]
 struct WorkflowExecution {
     id: String,
     name: String,
     status: String,
 }
 
-struct AppState {
-    executions: Mutex<Vec<WorkflowExecution>>, 
+fn get_workflows() -> Vec<WorkflowExecution> {
+    let bucket = store::open("default").unwrap();
+    let storage: Option<Vec<u8>> = bucket.get("workflows").unwrap();
+    drop(bucket);
+    
+    if let Some(data) = storage {
+        match serde_json::from_slice::<Vec<WorkflowExecution>>(&data) {
+            Ok(workflows) => workflows, 
+            Err(_) => {
+                Vec::new()
+            }
+        }
+    } else {
+        Vec::new()
+    }
 }
 
-// Utilisation de `lazy_static` pour un état global
-lazy_static! {
-    static ref APP_STATE: Arc<AppState> = Arc::new(AppState {
-        executions: Mutex::new(Vec::new()),
-    });
+fn save_worklfows( workflows :Vec<WorkflowExecution>){
+    let bucket = store::open("default").unwrap();
+    let workflows = serde_json::to_string(&workflows).unwrap();
+    let workflows_bytes : Vec<u8> = workflows.into_bytes();
+    let _ = bucket.set("workflows", &workflows_bytes);
+    drop(bucket);
 }
 
-fn create_workflow(workflow: Workflow, app_state: &AppState) -> String {
+fn create_workflow(workflow: Workflow) -> String {
     let workflow_id = Uuid::new_v4();
 
     let execution = WorkflowExecution {
@@ -48,10 +60,9 @@ fn create_workflow(workflow: Workflow, app_state: &AppState) -> String {
         status: "pending".to_string(),
     };
 
-    {
-        let mut executions = app_state.executions.lock().unwrap();
+    let mut executions = get_workflows();
         executions.push(execution.clone());
-    }
+        save_worklfows(executions);
 
     for task in workflow.task {
         match task.tasktype.as_str() {
@@ -73,47 +84,39 @@ fn create_workflow(workflow: Workflow, app_state: &AppState) -> String {
             }
             _ => {
                 info!("Tâche inconnue: {}", task.tasktype);
+               return  format!("Workflow non exécuté avec succès")
             }
         }
     }
 
-    {
-        let mut executions = app_state.executions.lock().unwrap();
-        if let Some(ex) = executions.iter_mut().find(|e| e.id == execution.id) {
-            ex.status = "completed".to_string();
-        }
-    }
+    let mut executions = get_workflows();
+    if let Some(pos) = executions.iter_mut().find(|e| e.id == workflow_id.to_string()) {
+        pos.status = "completed".to_string();
+        save_worklfows(executions);
+    } 
 
-    format!(
-        "Workflow {} exécuté avec succès",
-        workflow_id
-    )
+    format!( "Workflow {} exécuté avec succès", workflow_id)
 }
 
-fn get_executions(app_state: &AppState) -> Vec<WorkflowExecution> {
-    let executions = app_state.executions.lock().unwrap();
-    executions.clone()
-}
-
-fn get_workflow(path: &str, app_state: &AppState) -> Option<WorkflowExecution> {
+fn get_workflow(path: &str) -> Option<WorkflowExecution> {
     let path_parts: Vec<&str> = path.split('/').collect();
     let id = path_parts.last()?; 
 
-    let executions = app_state.executions.lock().unwrap();
+    let executions = get_workflows();
     executions.iter().find(|e| e.id == *id).cloned()
 }
 
-fn delete_workflow(path: &str, app_state: &AppState) -> Option<String> {
+fn delete_workflow(path: &str) -> Option<String> {
     let path_parts: Vec<&str> = path.split('/').collect();
     let id = path_parts.last()?; 
 
-    let mut executions = app_state.executions.lock().unwrap();
+    let mut executions = get_workflows();
 
     if let Some(pos) = executions.iter().position(|e| e.id == *id) {
         let removed_execution = executions.remove(pos);
+        save_worklfows(executions);
         return Some(format!("Workflow {} supprimé avec succès", removed_execution.id));
     }
-
     None
 }
 
@@ -125,10 +128,10 @@ impl Server for Component {
     fn handle(
         request: Request<IncomingBody>,
     ) -> Result<Response<impl OutgoingBody>, ErrorCode> {
+
         match (request.method().as_str(), request.uri().path()) {
             ("POST", "/workflows") => {
                 let (_parts, mut body) = request.into_parts();
-
                 let mut body_bytes = Vec::new();
                 body.read_to_end(&mut body_bytes).map_err(|_| ErrorCode::InternalError(Some("Invalid JSON".to_string())))?;
 
@@ -137,19 +140,21 @@ impl Server for Component {
                     Err(_) => {
                         return Ok(http::Response::builder()
                             .status(400)
+                            .header("Content-Type", "application/json")
                             .body("Invalid JSON".to_string())
                             .unwrap());
                     }
                 };
 
-                let result = create_workflow(body, &APP_STATE);
+                let result = create_workflow(body);
                 Ok(http::Response::builder()
                     .status(200)
+                    .header("Content-Type", "application/json")
                     .body(result)
                     .unwrap())
             }
-            ("GET", path) if path.starts_with("/workflow/") => {
-                match get_workflow(path, &APP_STATE) {
+            ("GET", path) if path.starts_with("/workflows/") => {
+                match get_workflow(path) {
                     Some(workflow) => {
                         let body = serde_json::to_string(&workflow).unwrap();
                         Ok(http::Response::builder()
@@ -160,33 +165,38 @@ impl Server for Component {
                     }
                     None => Ok(http::Response::builder()
                         .status(404)
+                        .header("Content-Type", "application/json")
                         .body("Workflow not found".to_string())
                         .unwrap()),
                 }
             }
-            ("DELETE", path) if path.starts_with("/workflow/") => {
-                match delete_workflow(path, &APP_STATE) {
+            ("DELETE", path) if path.starts_with("/workflows/") => {
+                match delete_workflow(path) {
                     Some(message) => Ok(http::Response::builder()
                         .status(200)
+                        .header("Content-Type", "application/json")
                         .body(message)
                         .unwrap()),
                     None => Ok(http::Response::builder()
                         .status(404)
+                        .header("Content-Type", "application/json")
                         .body("Workflow not found".to_string())
                         .unwrap()),
                 }
             }
             ("GET", "/workflows") => {
-                let executions = get_executions(&APP_STATE);
+                let executions = get_workflows();
                 let result = serde_json::to_string(&executions).unwrap();
                 Ok(http::Response::builder()
                     .status(200)
+                    .header("Content-Type", "application/json")
                     .body(result)
                     .unwrap())
             }
             _ => {
                 Ok(http::Response::builder()
                     .status(404)
+                    .header("Content-Type", "application/json")
                     .body("Not found".to_string())
                     .unwrap())
             }
